87fca15fd221ece12de7a61fe3a6573fab5bc13a
Amber Lin <410103037@gms.ndhu.edu.tw>
2022-07-16 17:09:17 +0800

    first commit

---
+++ app/assets/js/all.js
@@ -0,0 +1,55 @@
+const swiper = new Swiper('.swiper', {
+  spaceBetween: 30,
+  centeredSlides: true,
+  loop: true,
+  autoplay: {
+    delay: 1500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+
+$(document).ready(() => {
+  $(".moveUp").on('click', function () {
+    //console.log('click');
+    //     $('html,body').scrollTop(0);
+    $("html,body").animate({ scrollTop: 0 }, 1000);
+  });
+  $(window).on('scrollTop',function() {
+    console.log($(this).scrollTop());
+    if ( $(this).scrollTop() > 200){
+      $('.moveUp').fadeIn("fast");
+    } else {
+      $('.moveUp').stop().fadeOut("fast");
+    }
+  });
+});
+
+
+// form validation  by 助教
+(function () {
+  "use strict";
+  var forms = document.querySelectorAll(".needs-validation");
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener(
+      "submit",
+      function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
